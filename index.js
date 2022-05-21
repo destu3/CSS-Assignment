04c0354639d50590ba99bf790cfc8eb663ad0da3
destu3 <obose03@gmail.com>
2022-05-21 13:52:12 +0100

    made some nice changes

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ function animeProfile(image, profile){
     this.profile = profile
 }
 
-const profile1 = new animeProfile("./images/nino2.jpg", "this is some subtext under an illustration or image")
-const profile2 = new animeProfile("./images/kaguya.jpg", "this is some subtext under an illustration or image")
-const profile3 = new animeProfile("./images/nino2.jpg", "this is some subtext under an illustration or image")
-const profile4 = new animeProfile("./images/nino2.jpg", "this is some subtext under an illustration or image")
-const profile5 = new animeProfile("./images/nino2.jpg", "this is some subtext under an illustration or image")
-const profile6 = new animeProfile("./images/nino2.jpg", "this is some subtext under an illustration or image")
+const shinobu = new animeProfile("https://i.pinimg.com/originals/c8/02/fc/c802fc2b8836ed22e293df7c4ec1b17e.jpg", "Shinobu Kochō : Demon Slayer")
+const kaguya = new animeProfile("./images/kaguya.jpg",  "Kaguya Shinomiya : Kaguya-sama, Love is War")
+const hinata = new animeProfile("https://i.pinimg.com/originals/c4/8a/98/c48a989c9cdba4704c34518503d0e91d.jpg", "Hinata Hyuga : Naruto Shippuden")
+const lucy = new animeProfile("https://i.pinimg.com/736x/2b/28/41/2b2841fd51b04ba564403965601e2b92.jpg", "Lucy Heartfilia : Fairy tail")
+const chizuru = new animeProfile("https://i.pinimg.com/originals/15/18/e5/1518e5c5507e02af17184cd1b9934bce.jpg", "Chizuru Ichinose : Rent a Girlfriend")
+const mai = new animeProfile("https://i.pinimg.com/originals/12/01/84/12018463bbf253b1e402122a0babd25d.jpg", "Mai Sakurajima : Bunny Girl Senpai")
 
-const animeCharacters = [profile1,profile2,profile3,profile4,profile5,profile6];
+const animeCharacters = [shinobu,kaguya,hinata,lucy,chizuru,mai];
 
 animeCharacters.forEach(animeCharacter => {
     const newDiv = document.createElement("div")
